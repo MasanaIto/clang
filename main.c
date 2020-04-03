7fb0a2237d914dfb2a6a4a4eda18d1659e7cb4e9
MasanaIto <z3heng0326@gmail.com>
2020-04-03 19:19:15 +0900

    複数の整数値の合計と平均を求める

---
+++ main.c
@@ -0,0 +1,33 @@
+//
+//  main.c
+//  clang
+//
+//  Created by Masana on 2020/04/03.
+//  Copyright © 2020 伊藤政名. All rights reserved.
+//
+
+#include <stdio.h>
+ 
+int main()
+{
+    int sum = 0;
+    int cnt = 0;
+    int retry;
+    
+    do {
+        int t;
+        
+        printf("正数値を入力してください。：");
+        scanf("%d", &t);
+        
+        sum = sum + t;
+        cnt = cnt + 1;
+        
+        printf("まだ？<Yes...0/No...9>：");
+        scanf("%d", &retry);
+    } while (retry == 0);
+    
+    printf("合計は%dで平均は%.2fです。\n", sum, (double)sum / cnt);
+    
+    return 0;
+}
