dcaaa2dcf9b555c1e2f36e076056d30807ba29f0
MasanaIto <z3heng0326@gmail.com>
2020-04-04 23:32:59 +0900

    九九の表を表示

--- main.c
+++ main.c
@@ -1,27 +1,18 @@
 /*
-    読み込んだ正の整数値を逆順に表示
+    九九の表を表示
 */
 
 #include <stdio.h>
  
 int main(void)
 {
-    int no;
+    int i, j;
     
-    do {
-        printf("正の整数を入力してください。：");
-        scanf("%d", &no);
-        if (no <= 0)
-            puts("\a正でない数を入力しないでください。");
-    } while (no <= 0);
-    
-    /* noは0より大きくなっている */
-    printf("その数を逆から読むと");
-    while (no >0) {
-        printf("%d", no % 10);      /* 再開の桁の値を表示 */
-        no /= 10;                   /* 右に１桁ずらす */
+    for (i = 1; i <= 9; i++) {
+        for (j = 1; j <= 9; j++)
+            printf("%3d", i * j);
+        putchar('\n');
     }
-    puts("です。");
     
     return 0;
 }
